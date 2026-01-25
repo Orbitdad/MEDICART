e5c61bc2aeb1fdc04bfb376844c5e2c9597a833c
Adarsh Suresh Mandavkar <mandavkaradarsh2005@gmail.com>
2026-01-26 03:35:35 +0530

    feat: implement admin routes for medicine management with authentication

--- medi-backend/routes/medicineRoutes.js
+++ medi-backend/routes/medicineRoutes.js
@@ -1,8 +1,35 @@
 import express from "express";
-import { getMedicines } from "../controllers/medicineController.js";
+import {
+  adminGetMedicines,
+  adminCreateMedicine,
+  adminUpdateMedicine,
+  adminDeleteMedicine,
+} from "../controllers/medicineController.js";
+
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", getMedicines);
+/* ADMIN MEDICINE ROUTES */
+
+router.get("/medicines", protect(["admin"]), adminGetMedicines);
+
+router.post(
+  "/medicines",
+  protect(["admin"]),
+  adminCreateMedicine
+);
+
+router.put(
+  "/medicines/:id",
+  protect(["admin"]),
+  adminUpdateMedicine
+);
+
+router.delete(
+  "/medicines/:id",
+  protect(["admin"]),
+  adminDeleteMedicine
+);
 
 export default router;
