a5297eeb1eb1abd1eef0899c9606d2828f2d4905
Adarsh Suresh Mandavkar <mandavkaradarsh2005@gmail.com>
2026-01-26 05:33:37 +0530

    rou

--- medi-backend/routes/medicineRoutes.js
+++ medi-backend/routes/medicineRoutes.js
@@ -1,23 +1,11 @@
 import express from "express";
-import {
-  adminGetMedicines,
-  adminCreateMedicine,
-  adminUpdateMedicine,
-  adminDeleteMedicine,
-} from "../controllers/medicineController.js";
-
-import { protect } from "../middleware/authMiddleware.js";
+import { getMedicines } from "../controllers/medicineController.js";
 
 const router = express.Router();
 
-/* ADMIN MEDICINE ROUTES */
-
-router.get("/", protect(["admin"]), adminGetMedicines);
-
-router.post("/", protect(["admin"]), adminCreateMedicine);
-
-router.put("/:id", protect(["admin"]), adminUpdateMedicine);
-
-router.delete("/:id", protect(["admin"]), adminDeleteMedicine);
+router.get("/", getMedicines);
 
 export default router;
+
+
+
